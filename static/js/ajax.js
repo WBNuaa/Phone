97013611e2fdbd8d6dffd1f35af8e5175ff45942
bujidao1 <3218385100@qq.com>
2019-12-15 21:16:19 +0800

    Create ajax.js

---
+++ static/js/ajax.js
@@ -0,0 +1,17 @@
+<script type = "text/javascript" src = "/static/js/jquery-3.4.1.js">
+$.ajax(
+{
+    url: "http://127.0.0.1:5000/",
+    type:"POST",
+    dataType: "json",
+    url: "/aaa",// 后端请求
+    data: data,
+    success: function(result) {
+
+    }
+
+    error: function(
+
+    }
+}
+</script>
